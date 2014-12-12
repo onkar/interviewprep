699fc257cd09657fdd3c7863392df08ea066b364
Onkar Deshpande <onkar.deshpande@gmail.com>
2014-12-11 23:36:33 -0800

    Added documentation

--- DynamicProgramming/src/main/LongestCommonSubstring.java
+++ DynamicProgramming/src/main/LongestCommonSubstring.java
@@ -12,16 +12,33 @@ public class LongestCommonSubstring {
   private int[][] LCSuffix;
   private int substringLength;
 
+  /**
+   * <pre>
+   * 
+   *                     __ LCSuffix(i - 1, j - 1) + 1 if (s1.charAt(i - 1) == s2.charAt(j - 1))
+   *                    |
+   * LCSuffix(i, j) = --
+   *                    |__ 0 otherwise
+   *
+   * </pre>
+   * 
+   * @param s1
+   * @param s2
+   * @return
+   */
   public int getLength(String s1, String s2) {
+    // Allocate one extra row and one extra column
     LCSuffix = new int[s1.length() + 1][s2.length() + 1];
     substringLength = 0;
 
     for (int i = 0; i <= s1.length(); i++) {
       for (int j = 0; j <= s2.length(); j++) {
-        if (i == 0 || j == 0)
+        if (i == 0 || j == 0) {
+          // Set 0th row and 0th column to 0
           LCSuffix[i][j] = 0;
-        else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+        } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
           LCSuffix[i][j] = LCSuffix[i - 1][j - 1] + 1;
+          // Update substring length
           substringLength = Math.max(substringLength, LCSuffix[i][j]);
         } else {
           LCSuffix[i][j] = 0;
