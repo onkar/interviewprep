583894f294cc57eecbebc2fca3373e3e6eefb33b
Onkar Deshpande <onkar.deshpande@gmail.com>
2014-12-11 22:49:06 -0800

    Fixed typo and added some documentation

--- DynamicProgramming/src/main/HouseColoring.java
+++ DynamicProgramming/src/main/HouseColoring.java
@@ -20,7 +20,7 @@ public class HouseColoring {
   }
 
   // We can paint ith house with Blue, Red or Green. So, the dynamic programming equation becomes
-  // cost[i, Red] = min (cost[i-1, Blue], cost[i-1, Red]) + cost of painting i with Red
+  // cost[i, Red] = min (cost[i-1, Blue], cost[i-1, Green]) + cost of painting i with Red
   // cost[i, Blue] = min (cost[i-1, Red], cost[i-1, Green]) + cost of painting i with Blue
   // cost[i, Green] = min (cost[i-1, Red], cost[i-1, Blue]) + cost of painting i with Green
   // And minimum cost = min(cost[n, Red], min(cost[n, Blue], cost[n, Green]))
@@ -28,10 +28,14 @@ public class HouseColoring {
     int rows = costMatrix.length;
     int cols = costMatrix[0].length;
     int[][] cost = new int[rows][cols];
+
+    // Copy 0th row as is in the result matrix
     for (int j = 0; j < cols; j++) {
       cost[0][j] = costMatrix[0][j];
     }
     for (int i = 1; i < rows; i++) {
+      // 0th column is Red, 1st is Green and 2nd is Blue. NOTE: costMatrix is an input matrix while
+      // cost matrix is the result matrix
       cost[i][0] = Math.min(cost[i - 1][1], cost[i - 1][2]) + costMatrix[i][0];
       cost[i][1] = Math.min(cost[i - 1][0], cost[i - 1][2]) + costMatrix[i][1];
       cost[i][2] = Math.min(cost[i - 1][0], cost[i - 1][1]) + costMatrix[i][2];
