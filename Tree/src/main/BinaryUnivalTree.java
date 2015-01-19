76da137b63249ea3c17ce21d6bc5bdf22c91ee95
Onkar Deshpande <onkar.deshpande@gmail.com>
2015-01-18 20:24:23 -0800

    Iterative version of binary unival tree

--- Tree/src/main/BinaryUnivalTree.java
+++ Tree/src/main/BinaryUnivalTree.java
@@ -1,20 +1,37 @@
 package main;
 
+import java.util.Stack;
+
 import main.BinarySearchTree.BSTNode;
 
 public class BinaryUnivalTree {
-  public boolean isUnival(BSTNode root) {
+  public boolean isUnivalRecursive(BSTNode root) {
     if (root == null || root.left == null && root.right == null)
       return true;
 
     if (root.left != null && root.right == null && root.data == root.left.data)
-      return isUnival(root.left);
+      return isUnivalRecursive(root.left);
 
     if (root.right != null && root.left == null && root.data == root.right.data)
-      return isUnival(root.right);
+      return isUnivalRecursive(root.right);
 
-    return root.data == root.left.data && root.data == root.right.data && isUnival(root.left)
-        && isUnival(root.right);
+    return root.data == root.left.data && root.data == root.right.data
+        && isUnivalRecursive(root.left) && isUnivalRecursive(root.right);
+  }
+
+  public boolean isUnivalIterative(BSTNode root) {
+    Stack<BSTNode> stack = new Stack<BSTNode>();
+    stack.push(root);
+    while (!stack.isEmpty()) {
+      BSTNode node = stack.pop();
+      if (node.data != root.data)
+        return false;
+      if (node.left != null)
+        stack.push(node.left);
+      if (node.right != null)
+        stack.push(node.right);
+    }
+    return true;
   }
 
   public static int count;
